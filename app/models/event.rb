a7985270020dd59e9034825886b5dfe6402781e7
Viktor Miric <viktorm47@gmail.com>
2023-05-27 16:16:31 +0200

    Added association between event-user as attendee

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
 
+  has_many :event_attendees, foreign_key: "event_id"
+  has_many :attendees, through: :event_attendees
+
   validates :title, presence: true, length: { minimum: 5 }
   validates :description, presence: true    
   validates :date, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_27_104326) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_27_133647) do
+  create_table "event_attendees", id: false, force: :cascade do |t|
+    t.integer "event_id"
+    t.integer "attendee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attendee_id"], name: "index_event_attendees_on_attendee_id"
+    t.index ["event_id"], name: "index_event_attendees_on_event_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
@@ -35,5 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_27_104326) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "event_attendees", "events"
+  add_foreign_key "event_attendees", "users", column: "attendee_id"
   add_foreign_key "events", "users", column: "creator_id"
 end
