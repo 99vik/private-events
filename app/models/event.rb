f41b9cf3224e2547feb36d6ab03f00b0bc29a87f
Viktor Miric <viktorm47@gmail.com>
2023-05-27 12:12:26 +0200

    Added event model with relation to creator(user)

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  belongs_to :creator, class_name: 'User'
+
   validates :title, presence: true, length: { minimum: 5 }
   validates :description, presence: true    
   validates :date, presence: true
